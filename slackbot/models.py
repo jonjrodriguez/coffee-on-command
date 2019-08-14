9a04015bde4950ccba7ab172d35fdbe53fbfc426
Jonathan Rodriguez <jonathan@cadre.com>
2019-08-14 12:10:49 -0400

    Add models

--- slackbot/models.py
+++ slackbot/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class CoffeeRequest(models.Model):
+    user_id = models.CharField(max_length=255)
+    response_url = models.CharField(max_length=255)
+    matches = models.ForeignKey(
+        to="slackbot.Match", on_delete=models.CASCADE, related_name="coffee_request"
+    )
+
+
+class Match(models.Model):
+    user_id = models.CharField(max_length=255)
+    is_accepted = models.BooleanField(null=True)
+    expiration = models.DateTimeField()
