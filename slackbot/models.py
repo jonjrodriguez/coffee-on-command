b0a2b5e0e000fe335209d1a2dd2b8f89172e589b
guy <guymaor86@gmail.com>
2019-08-14 15:42:43 -0400

    add created for cofee request

--- slackbot/models.py
+++ slackbot/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 class CoffeeRequest(models.Model):
     user_id = models.CharField(max_length=255)
     response_url = models.CharField(max_length=255)
+    created = models.DateTimeField(default=now)
 
 
 class Match(models.Model):
