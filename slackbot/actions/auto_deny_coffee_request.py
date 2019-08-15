38ef68a646c736785dee26c7c8f598ca2e8baddb
Doris Ip <doris@cadre.com>
2019-08-14 23:24:03 -0400

    One place for messaging match user and when there is no match

--- slackbot/actions/auto_deny_coffee_request.py
+++ slackbot/actions/auto_deny_coffee_request.py
@@ -3,7 +3,7 @@ from .base import Action
 
 class AutoDenyCoffeeRequest(Action):
     def execute(self, *, user_id, block_id, ts, channel):
-        self.matcher.deny_request(user_id, block_id)
+        denied_match = self.matcher.deny_request(user_id, block_id)
         self.client.update(
             channel=channel,
             ts=ts,
@@ -21,19 +21,7 @@ class AutoDenyCoffeeRequest(Action):
                 },
             ]
         )
+
         # Create new request
-        # coffee_request = denied_match.coffee_request
-        # match = self.matcher.create_match(coffee_request)
-        # if match:
-        #     self.client.send_invite(receiver_id=match.user_id, block_id=match.block_id)
-        #     return
-        #
-        # self.client.post_to_private(
-        #     receiver_id=coffee_request.user_id,
-        #     blocks=[
-        #         {
-        #             "type": "section",
-        #             "text": {"type": "mrkdwn", "text": "No coffee buddies found"},
-        #         }
-        #     ],
-        # )
+        coffee_request = denied_match.coffee_request
+        self.matcher.create_match(coffee_request)
